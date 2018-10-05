78aa20d7549f2fa88c3cb68da029cf2509826e0a
Irvin Naufal <42725269+irvinnaufal@users.noreply.github.com>
2018-10-05 17:18:20 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("db.php");
+$id = $_GET["nim"];
+$sql = "DELETE FROM siswa WHERE nim = $id";
+if (mysqli_query($conn, $sql)) {
+    echo "Data berhasil dihapus";
+}else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+?>
