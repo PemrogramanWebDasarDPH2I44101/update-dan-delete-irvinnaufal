e1d613867600405015d14edd1685ce6e2604523b
Irvin Naufal <42725269+irvinnaufal@users.noreply.github.com>
2018-10-05 17:17:56 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,10 @@
 <?php
 $servername = "localhost";
-$username   = "admin";
-$password   = "1234";
-$db         = "WebDasar";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+$username = "root";
+$password = "";
+$db = "webdas";
+$conn = mysqli_connect($servername, $username, $password,$db);
+if (! $conn) {
+	die("Koneksi Gagal: " . mysqli_connect_error());
 }
 ?>
